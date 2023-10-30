70aac00e449baf4149b5c374e90c3f4954420776
Dimitar Varzilov <dnvarzilov@diadraw.com>
2023-10-30 18:49:45 +0200

    added no whitespace to password regex

--- AuthenticationAPI/Dto/LoginDto.cs
+++ AuthenticationAPI/Dto/LoginDto.cs
@@ -10,7 +10,7 @@ namespace AuthenticationAPI.Dto
 
 		[Required(ErrorMessage = "Password is required")]
 		[DataType(DataType.Password)]
-		[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$", ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.")]
+		[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\d\sa-zA-Z]).{8,}$", ErrorMessage = "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character.")]
 		public string Password { get; set; } = string.Empty;
 	}
 }
